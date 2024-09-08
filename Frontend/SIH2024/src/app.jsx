f257220f116da0414e495f6c4a7041380eedabfc
Gopinathv19 <gopinath5166296@gmail.com>
2024-09-08 13:58:52 +0530

    The Route Page is arranged

--- Frontend/SIH2024/src/app.jsx
+++ Frontend/SIH2024/src/app.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./supervisor/pages/Component/Home";
-import Doctors from "./supervisor/pages/Component/PatientReport";
-import AllocationRequests from "./supervisor/pages/Component/AllocationRequests";
-import Profile from "./supervisor/pages/Component/StudentTherapistProfile";
-import Logout from "./supervisor/pages/Component/Logout";
- 
-import UpdateProfile from "./supervisor/pages/Component/UpdateProfile";
 
-import PatientFeedback from "./supervisor/pages/Component/PatientFeedback";
-import ReviewTherapyPlan from "./supervisor/pages/Component/ReviewTherapyPlan";
-import PatientSummary from "./supervisor/pages/Component/PatientSummary";
-import SupervisorResources from "./supervisor/pages/Component/SupervisorResources";
- 
-import AuthPage from "./Doctor/pages/auth";
+// Patient Components
 import PatientAuthPage from "./Patient/components/auth";
-import SupervisorAuthPage from "./supervisor/pages/Component/Auth";
-import RoleSelection from "./authbutton";
-import ProgressSummary from "./supervisor/pages/Component/PatientStatusUpdate";
 import LandingPage from "./Patient/components/landing";
-import Dashboard from "./supervisor/pages/Component/Dashboard";
-import Dboard from "./Doctor/component/doctordash";
 import AppointmentForm from "./Patient/components/appointment";
-import Pending from "./supervisor/pages/Component/PatientPendingRequests";
-import PatientUpdate from "./supervisor/pages/Component/PatientStatusUpdate";
-import PatientDetail from "./supervisor/pages/Component/ViewSummary";
-import PatientReport from "./supervisor/pages/Component/PatientReport";
-import StudentTherapistReport from "./Doctor/pages/StudentTherapistReport";
+import PatientSummary from "./StudentTherapist/pages/Component/PatientSummary";
+import PatientUpdate from "./StudentTherapist/pages/Component/PatientStatusUpdate";
+import PatientDetail from "./StudentTherapist/pages/Component/ViewSummary";
+import PatientReport from "./StudentTherapist/pages/Component/PatientReport";
+
+// StudentTherapist Components
+import RoleSelection from "./authbutton";
+import AllocationRequests from "./StudentTherapist/pages/Component/AllocationRequests";
+import Profile from "./StudentTherapist/pages/Component/StudentTherapistProfile";
+import Logout from "./StudentTherapist/pages/Component/Logout";
+import UpdateProfile from "./StudentTherapist/pages/Component/UpdateProfile";
+import PatientFeedback from "./StudentTherapist/pages/Component/PatientFeedback";
+import ReviewTherapyPlan from "./StudentTherapist/pages/Component/ReviewTherapyPlan";
+import SupervisorResources from "./StudentTherapist/pages/Component/SupervisorResources";
+import ProgressSummary from "./StudentTherapist/pages/Component/PatientStatusUpdate";
+import Dashboard from "./StudentTherapist/pages/Component/Dashboard";
+import Pending from "./StudentTherapist/pages/Component/PatientPendingRequests";
+import StudentTherapistReport from "./TherapistSupervisor/pages/StudentTherapistReport";
+
+// TherapistSupervisor Components
+import AuthPage from "./TherapistSupervisor/pages/auth";
+import SupervisorAuthPage from "./StudentTherapist/pages/Component/auth";
+import Dboard from "./TherapistSupervisor/component/therapistSupervisorDash";
+
 // Import the Notification component if it exists
 // import Notification from "./path/to/Notification";
 
@@ -34,15 +36,18 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Patient Routes */}
         <Route path="/" element={<RoleSelection />} />
-        <Route path="/navbar" element={<Navbar />} />
-        <Route path="/doctorauth" element={<AuthPage />} />
-        <Route path="/suprevisorauth" element={<SupervisorAuthPage />} />
-        <Route path="/patient/:id" component={<PatientSummary />} />
         <Route path="/patientauth" element={<PatientAuthPage />} />
-        <Route path="/notifications" element={<Notification />} />
- 
-        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/patientdash" element={<LandingPage />} />
+        <Route path="/appointment" element={<AppointmentForm />} />
+        <Route path="/patient/:id" element={<PatientSummary />} />
+        <Route path="/PatientUpdate/:id" element={<PatientUpdate />} />
+        <Route path="/summary/:id" element={<PatientDetail />} />
+        <Route path="/patientreport" element={<PatientReport />} />
+        <Route path="/sendingtodoctor/:id" element={<PatientReport />} />
+
+        {/* StudentTherapist Routes */}
         <Route path="/allocation-requests" element={<AllocationRequests />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/logout" element={<Logout />} />
@@ -50,34 +55,19 @@ function App() {
         <Route path="/monitor-progress" element={<PatientSummary />} />
         <Route path="/patient-feedback" element={<PatientFeedback />} />
         <Route path="/review-plans" element={<ReviewTherapyPlan />} />
-
         <Route path="/training-resources" element={<SupervisorResources />} />
         <Route path="/progress-summary" element={<ProgressSummary />} />
-        <Route path="/patientdash" element={<LandingPage />} />
         <Route path="/supervisordash" element={<Dashboard />} />
+        <Route path="/pending-requests" element={<Pending />} />
+        <Route path="/StudentTherapistReport" element={<StudentTherapistReport />} />
+
+        {/* TherapistSupervisor Routes */}
+        <Route path="/doctorauth" element={<AuthPage />} />
+        <Route path="/suprevisorauth" element={<SupervisorAuthPage />} />
         <Route path="/doctordash" element={<Dboard />} />
-        <Route
-          path="/pending-requests"
-          element={<Pending />}
-        />
-        <Route path="/appoinment" element={<AppointmentForm />} />
-        <Route path="/pendingrequest" element={<Pending />} />
-        <Route path="/PatientUpdate/:id" element={<PatientUpdate />} />
 
-        <Route path="/summary/:id" element={<PatientDetail />} />
-
-
-
-        <Route path="/patientreport" element={<PatientReport />} />
-
-
-        <Route path="/sendingtodoctor/:id" element={<PatientReport />} />
-
-
-        <Route
-          path="/StudentTherapistReport"
-          element={<StudentTherapistReport />}
-        />
+        {/* Optional Route */}
+        {/* <Route path="/notifications" element={<Notification />} /> */}
       </Routes>
     </Router>
   );
